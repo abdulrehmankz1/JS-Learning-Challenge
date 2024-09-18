9f74c60f16e41acf5c24ca795f9b2b661016ed3e
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-09-18 21:21:52 +0500

    add primitive data types

---
+++ firstClass.js
@@ -0,0 +1,23 @@
+// 1. Primitive Data Types
+
+// String - Text values
+let myString = "Hello, World!";
+console.log("String:", myString);
+
+// Number - Numeric values
+let myNumber = 42;
+console.log("Number:", myNumber);
+
+// Boolean - True or False values
+let isLearning = true;
+console.log("Boolean:", isLearning);
+
+// Null - Represents "nothing" or empty value
+let myNull = null;
+console.log("Null:", myNull);
+
+// Undefined - Variable declared but not assigned a value
+let myUndefined;
+console.log("Undefined:", myUndefined);
+
+
