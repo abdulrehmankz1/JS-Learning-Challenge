573f918d4ee6d549686d18efe2cbacc5846c8907
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-09-20 21:17:10 +0500

    add opraters

---
+++ SecondClass.js
@@ -0,0 +1,5 @@
+// Operators in JavaScript
+// Operator: An operator is a special symbol used to perform operations on one, two, or three operands,
+//  resulting in a value.Operators are essential for manipulating data and variables in JavaScript.
+
+
