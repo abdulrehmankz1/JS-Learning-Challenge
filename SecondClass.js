910068cc8e4552ced932d029fcb5a48bcbc34753
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-09-20 21:18:26 +0500

    add arithmetic opraters

--- SecondClass.js
+++ SecondClass.js
@@ -3,3 +3,8 @@
 //  resulting in a value.Operators are essential for manipulating data and variables in JavaScript.
 
 
+// 1. Arithmetic Operators
+// Definition: Arithmetic operators are used to perform mathematical calculations such as addition,
+//  subtraction, multiplication, and division.
+
+
