b1c71ad9479c947518d9fed0c351f577bf3dd61d
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-09-24 20:02:37 +0500

    add control flow statements defination

---
+++ thirdClass.js
@@ -0,0 +1,3 @@
+// Control Flow Statements:
+// Definition: Control flow statements direct the order in which code is executed,
+// allowing decisions to be made and different actions to occur based on conditions.
