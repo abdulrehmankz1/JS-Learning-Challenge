605c095533b83a40f04863ce3f6efaa6dee70501
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-09-24 20:03:47 +0500

    add control flow statements example

--- thirdClass.js
+++ thirdClass.js
@@ -1,3 +1,11 @@
 // Control Flow Statements:
 // Definition: Control flow statements direct the order in which code is executed,
 // allowing decisions to be made and different actions to occur based on conditions.
+
+let age = 20;
+
+if (age >= 18) {
+    console.log("You are eligible to vote.");
+} else {
+    console.log("You are not eligible to vote.");
+}
