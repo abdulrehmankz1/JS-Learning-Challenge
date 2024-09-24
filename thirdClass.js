82052dc5452fab748678120c5681cd4a3e96d608
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-09-24 20:05:19 +0500

    add if-else example

--- thirdClass.js
+++ thirdClass.js
@@ -2,6 +2,16 @@
 // Definition: Control flow statements direct the order in which code is executed,
 // allowing decisions to be made and different actions to occur based on conditions.
 
+// Example 1: Basic if Statement
+
+let number = 10;
+
+if (number > 5) {
+    console.log("The number is greater than 5.");
+}
+
+// Example 2: if-else Statement
+
 let age = 20;
 
 if (age >= 18) {
