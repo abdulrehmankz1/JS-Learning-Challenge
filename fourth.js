8c629636a7c6fb67fc45a72b261af15c94f9d914
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-09-27 19:58:22 +0500

    add do..while loop example

--- fourth.js
+++ fourth.js
@@ -7,7 +7,7 @@
 // A for loop is used when you know how many times you want to repeat a block of code.
 
 for (let i = 1; i <= 10; i++) {
-    console.log(i);
+  console.log(i);
 }
 
 // while Loop:
@@ -15,9 +15,15 @@ for (let i = 1; i <= 10; i++) {
 
 let i = 1;
 while (i <= 10) {
-    console.log(i);
-    i++;
+  console.log(i);
+  i++;
 }
 
 // do...while Loop:
 // A do...while loop is similar to the while loop, but the block of code is executed at least once, even if the condition is false.
+
+let i = 1;
+do {
+  console.log(i);
+  i++;
+} while (i <= 10);
