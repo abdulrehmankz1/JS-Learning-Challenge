f308d9fdad69e1d15b70ee2bf03d4b5a02949219
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-09-27 19:28:15 +0500

    add loops defination

---
+++ fourth.js
@@ -0,0 +1,4 @@
+// Loops in JavaScript:
+// Definition: Loops are used to repeatedly execute a block of code
+// as long as a specified condition is true. They are useful for tasks that require repetition,
+// like printing numbers or going through arrays.
