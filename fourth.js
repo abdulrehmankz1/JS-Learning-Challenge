0477a707be1ff4ae0d9b8bb3dbd770ae5394c559
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-09-27 19:34:46 +0500

    add for loop defination

--- fourth.js
+++ fourth.js
@@ -2,3 +2,7 @@
 // Definition: Loops are used to repeatedly execute a block of code
 // as long as a specified condition is true. They are useful for tasks that require repetition,
 // like printing numbers or going through arrays.
+
+// for Loop:
+// A for loop is used when you know how many times you want to repeat a block of code.
+
