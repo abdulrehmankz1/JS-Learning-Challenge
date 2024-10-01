a7f4266a0980fc506b98316ecde1f7b4e9c349a2
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-10-01 16:57:50 +0500

    add Function Declaration with example

--- fifthClass.js
+++ fifthClass.js
@@ -3,4 +3,12 @@
 // Functions can take inputs (parameters), process them, and return an output (or result).
 // Functions help in making code reusable and organized.
 
+// In JavaScript, there are primarily three types of functions:
 
+// 1. Function Declaration:
+// This is the traditional way of defining a function with a specific name.
+
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+greet("Abdul");
