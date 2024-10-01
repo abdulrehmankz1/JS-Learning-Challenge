789116331a86fd5dd2bb4563f1e1cff0cc94dad0
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-10-01 16:57:01 +0500

    add functions defination

--- fifthClass.js
+++ fifthClass.js
@@ -2,3 +2,5 @@
 // Definition: A function is a block of code designed to perform a specific task.
 // Functions can take inputs (parameters), process them, and return an output (or result).
 // Functions help in making code reusable and organized.
+
+
