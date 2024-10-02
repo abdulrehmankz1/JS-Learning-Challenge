b9deedd4fb413b7a6d73654fe5af159ee6c20085
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-10-02 19:34:36 +0500

    add array example

--- class-6.js
+++ class-6.js
@@ -1,3 +1,6 @@
 // Arrays in JavaScript:
 // Definition: An array is a data structure that can hold multiple values in a single variable.
 // Arrays are used to store a collection of data, which can be accessed by their index (starting from 0).
+
+let fruits = ["apple", "banana", "mango"];
+console.log(fruits);
