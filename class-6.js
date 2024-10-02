6c6cefbb6396251fb997acb00003fa21ba7849ec
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-10-02 19:32:38 +0500

    add array defination

---
+++ class-6.js
@@ -0,0 +1,3 @@
+// Arrays in JavaScript:
+// Definition: An array is a data structure that can hold multiple values in a single variable.
+// Arrays are used to store a collection of data, which can be accessed by their index (starting from 0).
