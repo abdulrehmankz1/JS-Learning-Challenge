2814a9a7fd5cc11ebdea9582c63c84effef112cd
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-10-04 15:05:03 +0500

    add object defination

---
+++ class-7.js
@@ -0,0 +1,5 @@
+// Objects in JavaScript:
+// Definition: An object in JavaScript is a collection of key-value pairs.
+// Each key (also called a property) has a value, which can be any type of data
+// (number, string, array, another object, or even a function). Objects are useful
+// for storing and managing data in a structured way.
