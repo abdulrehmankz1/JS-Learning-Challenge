b079371d9c705724d3048871456a63850b2045ba
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-10-04 15:16:35 +0500

    add object methods

--- class-7.js
+++ class-7.js
@@ -22,3 +22,25 @@ console.log(person);
 // Accessing properties
 console.log(person.name);  // Outputs: "Abdul Rehman" (dot notation)
 console.log(person["age"]);  // Outputs: 25 (bracket notation)
+
+
+// Object Methods:
+// A method in an object is a function stored as a property.
+// You can define functions inside an object and call them just like you would call object properties.
+
+let user = {
+    name: "Abdul Rehman",
+    age: 25,
+    city: "Karachi",
+
+    // Method to greet
+    greet: function() {
+        console.log("Hello, my name is " + this.name);
+    }
+};
+
+// Calling the method
+person.greet();  // Outputs: "Hello, my name is Abdul Rehman"
+
+// this keyword: In methods, this refers to the object itself,
+// so this.name accesses the name property of the object.
