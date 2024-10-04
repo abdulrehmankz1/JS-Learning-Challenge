51d264268e31ad550fd665f96a19014eb17153af
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-10-04 15:05:57 +0500

    add object creation

--- class-7.js
+++ class-7.js
@@ -3,3 +3,15 @@
 // Each key (also called a property) has a value, which can be any type of data
 // (number, string, array, another object, or even a function). Objects are useful
 // for storing and managing data in a structured way.
+
+// Object Creation:
+// You can create an object by using curly braces {} and defining key-value pairs inside.
+
+// Creating an object
+let person = {
+    name: "Abdul Rehman",
+    age: 25,
+    city: "Karachi"
+};
+
+console.log(person);
