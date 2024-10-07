bac84c0ddf6fcd5855f2aec38dec0ea198ebdd23
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-10-07 17:32:17 +0500

    add DOM Manipulation Defination

---
+++ class-8.js
@@ -0,0 +1,4 @@
+// DOM Manipulation:
+// DOM (Document Object Model) is a representation of the HTML structure of a
+// webpage that allows JavaScript to access, modify, and interact with elements dynamically.
+
